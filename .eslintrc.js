6b518829781b369cdece2b746a7d8a26c8419c1d
Ryan Rushton <ryan.rushton79@gmail.com>
2020-01-30 00:35:42 +1100

    Extracted ts eslint config out to my common eslint config project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,6 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ["./tsconfig.json"],
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    plugins: ["@typescript-eslint"],
     extends: [
         "@ryan-rushton/eslint-config",
-        "@ryan-rushton/eslint-config/react-prettier",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:import/typescript",
-        "prettier/@typescript-eslint"
-    ],
-    rules: {
-        "react/prop-types": "off",
-        "@typescript-eslint/typedef": [
-            "error",
-            {
-                arrowParameter: true,
-                memberVariableDeclaration: false,
-                parameter: true,
-                propertyDeclaration: true
-            }
-        ]
-    },
-    settings: {
-        "import/resolver": {
-            node: {
-                extensions: [".js", ".ts", ".jsx", ".tsx", ".json", ".module.scss$"]
-            }
-        },
-        "import/extensions": [".js", ".ts", ".jsx", ".tsx", ".module.scss$"]
-    }
+        "@ryan-rushton/eslint-config/typescript-react-prettier"
+    ]
 };
