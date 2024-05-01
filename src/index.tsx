ec5b069c3269955a315f4fdf6232ec763ffc98a5
Ryan <ryan.rushton79@gmail.com>
2024-05-02 08:08:21 +1000

    Update react things and prettier import sorting

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import AppWrapper from './AppWrapper';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppWrapper />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(<AppWrapper />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
